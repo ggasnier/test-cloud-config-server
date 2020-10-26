89d93c043f3bf2f078cce9d36e8306790ba25ded
ggasnier <23357977+ggasnier@users.noreply.github.com>
2020-10-26 09:50:28 +0100

    Initial commit

---
+++ src/main/java/com/gasnier/testcloudconfigserver/TestCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gasnier.testcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCloudConfigServerApplication.class, args);
+    }
+
+}
